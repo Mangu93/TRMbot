2904c82bf0a2b359c0d257bc46ccb44e3ad2913e
Mangu93 <adrianmarinportillo@gmail.com>
2016-04-18 08:29:37 +0000

    Added nmap, ping and speedtest (need nmap and speedtest dependencies)

--- raspbot.py
+++ raspbot.py
@@ -4,12 +4,13 @@ import telebot # Librería de la API del bot.
 from telebot import types # Tipos para la API del bot.
 import time # Librería para hacer que el programa que controla el bot no se acabe.
 import random
+import urllib2
 import datetime
 import token
 import os
 import commands
 import sched
-TOKEN = 'Enter your token' #Nuestro token del bot
+TOKEN = '' #Nuestro token del bot
 s = sched.scheduler(time.time, time.sleep)
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
 #############################################
@@ -26,7 +27,7 @@ bot.set_update_listener(listener) # Así, le decimos al bot que utilice como fun
 @bot.message_handler(commands=['help']) 
 def command_ayuda(m): 
     cid = m.chat.id 
-    bot.send_message( cid, "Comandos Disponibles: /help, /date, /temp, /saludo, /espacio")
+    bot.send_message( cid, "Comandos Disponibles: /help, /ping, /nmap,  /date, /temp, /saludo, /espacio")
 
 @bot.message_handler(commands=['temp'])
 def command_temp(m):
@@ -34,6 +35,30 @@ def command_temp(m):
 	#temp = os.system('sudo /opt/vc/bin/vcgencmd measure_temp')
 	temp = commands.getoutput('sudo /opt/vc/bin/vcgencmd measure_temp')	
 	bot.send_message(cid, temp)
+
+@bot.message_handler(commands=['camara'])
+def command_camara(m):
+	cid = m.chat.id
+	camara = commands.getoutput('bash /home/pi/camara.sh')
+	#img = urllib2.urlopen('home/pi/camara/shot').read()
+	bot.send_message(cid,camara)
+	photo = open('/home/pi/camara/shot')
+	bot.send_photo(cid,img)
+	commands.getouput('rm /home/pi/camara/shot')
+		
+
+@bot.message_handler(commands=['speedtest'])
+def command_speedtest(m):
+	cid = m.chat.id
+	speedtest = commands.getoutput('speedtest-cli')
+	bot.send_message(cid, speedtest)
+
+@bot.message_handler(commands=['ping'])
+def command_ping(m):
+	cid = m.chat.id
+	ping = commands.getoutput('sudo ping -c 4 8.8.8.8')
+	bot.send_message(cid,ping)
+	bot.send_message(cid,cid)  
 
 @bot.message_handler(commands=['date'])
 def command_temp(m):
@@ -45,7 +70,13 @@ def command_temp(m):
         cid = m.chat.id
 	saludo = commands.getoutput('bash /home/pi/saludo.sh')
         bot.send_message(cid, saludo)
-
+@bot.message_handler(commands=['nmap'])
+def command_nmap(m):
+	cid = m.chat.id
+	msg = "Este va a tardar"
+	bot.send_message(cid, msg)
+	info = commands.getoutput('sudo nmap -O -F 192.168.1.1/24')
+	bot.send_message(cid,info)
 @bot.message_handler(commands=['espacio'])
 def command_espacio(m):
         cid = m.chat.id
